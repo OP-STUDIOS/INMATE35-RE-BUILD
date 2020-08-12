9b04195032307d6d1972d6584974056262ad3388
Tom <tomtomcoromandel@gmail.com>
2020-08-12 13:05:05 +1000

    Setup
    Setting up the repository

---
+++ Assets/SlowMoBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMoBar : MonoBehaviour
+{
+    private float slowMo = 0f;
+
+    private void Start()
+    {
+        
+    }
+}
